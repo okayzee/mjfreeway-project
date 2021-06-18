e003126051bd0a2ec745aabe53245eb40270efaf
Kazeem Odofin <odofinkayzee01@gmail.com>
2021-06-18 08:37:42 -0500

    get coffe

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,55 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component lol</div>
+        <div class="row justify-content-center" v-for="coffees in coffee" :key="coffees.id">
 
+            <div class="col-md-4 card-space">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
                     <div class="card-body">
-                        I'm an example component again.
+                        <h5 class="card-title">{{ coffees.name }} - {{ coffees.serving }}mg</h5>
+                        <p class="card-text">{{ coffees.description }}</p>
+                        <a href="#" class="btn btn-primary">Buy</a>
                     </div>
                 </div>
             </div>
+
+
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        mounted() {
+        name: "coffee",
+        data() {
+            return {
+                coffee: {}
+            };
+        },
+        async mounted() {
             console.log('Component mounted.')
-        }
+            await this.getCoffee();
+        },
+        methods: {
+            async getCoffee() {
+                try {
+                    const {
+                        data: { data },
+                    } = await axios.get(
+                        `/api/coffee`
+                    );
+                    this.coffee = data;
+                    console.log(data)
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        },
     }
 </script>
+
+<style scoped>
+    .card-space {
+        margin-bottom: 20px;
+    }
+</style>
